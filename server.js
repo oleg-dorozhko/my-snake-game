167ac9be61c332ead9e79b078adfec4a520f59af
oleg-dorozhko <oleg.dorozhko@gmail.com>
2026-01-02 21:26:51 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const http = require('http');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000; 
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
